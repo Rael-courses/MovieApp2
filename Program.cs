8be6d82255c37a19980b58169dbd233e1139cc17
Rael CALITRO <rael06@hotmail.fr>
2025-11-28 10:08:57 +0100

    Add environment variable management with DotNetEnv integration
    - Introduced IEnvService and EnvService for handling environment variables.
    - Added validation for mandatory environment variables.
    - Integrated DotNetEnv package for loading environment variables.
    - Updated Program.cs to register EnvService in the dependency injection container.
    - Created interfaces IEnvService and IVariables for better abstraction.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MovieAppApi.Src.Core.Services.Environment;
+
 namespace MovieAppApi;
 
 public class Program
@@ -5,6 +7,8 @@ public class Program
   public static void Main(string[] args)
   {
     var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSingleton<IEnvService>(new EnvService());
+
     builder.Services.AddControllers();
 
     // Add Swagger/OpenAPI support
